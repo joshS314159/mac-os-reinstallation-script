d80a06f859d30326104bb7397be167a4cd535362
Joshua Sarver <joshua@netbeez.net>
2017-02-21 14:31:45 -0500

    update apps

--- pre_install.sh
+++ pre_install.sh
@@ -16,10 +16,17 @@ f_homebrew_setup(){
     brew bundle dump --force --file="$(pwd)/Brewfile"
 }
 
+f_back_to_usb_folder(){
+    readonly backup_dir="$HOME/Downloads/mac-os-reinstallation-package"
+    mkdir -p "$backup_dir"
 
+    cp -r ~/Documents/Sync/Mackup "$backup_dir/Mackup"
+    cp -r ~/Documents/Repositories/mac-os-reinstallation-script "$backup_dir/mac-os-reinstallation-script"
+}
 
 f_main(){
     f_sublime_files
     f_homebrew_setup
+    f_back_to_usb_folder
 }
 f_main
