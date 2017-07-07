ab698d2c5f77ec3b31b5cd4f95abad64eccc36b0
Joshua Sarver <joshua@netbeez.net>
2017-07-06 23:26:25 -0400

    backups and cleanup
    * backup arq from cli
    
    * backup prefs via mackup
    
    * remove back_to_usb_folder (just curl this stuff from git)

--- setup.sh
+++ setup.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
 
+function backup_arq(){
+    (
+        cd "/Applications/Arq.app/Contents/MacOS"
+        
+        # select B2
+        ./Arq backupnow
+        
+        # select local
+        ./Arq backupnow
+    )
+}
+
+
+function backup_mackup(){
+    mackup backup
+}
 
 
 function homebrew_setup(){
     brew bundle dump --force --file="$(pwd)/Brewfile"
 }
 
-function back_to_usb_folder(){
-    local -r backup_dir="$HOME/Downloads/mac-os-reinstallation-package"
-    mkdir -p "$backup_dir"
-
-    cp -r ~/Documents/Sync/Mackup "$backup_dir/Mackup"
-    cp -r ~/Documents/Repositories/mac-os-reinstallation-script "$backup_dir/mac-os-reinstallation-script"
-}
 
 function main(){
-    # sublime_files
+    backup_mackup
+    
     homebrew_setup
-    # back_to_usb_folder
+    
+    backup_arq
 }
 main
 
