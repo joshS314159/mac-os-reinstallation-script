afa308d0f62387f8886fa3a7a6f79f9ab7f7143b
Joshua Sarver <joshua@netbeez.net>
2018-08-23 20:19:44 -0400

    clean up preinstall

--- pre_install.sh
+++ pre_install.sh
@@ -6,59 +6,53 @@ set -o pipefail #exit script if anything fails in pipe
 # set -x;       # debug mode
 
 
-#########################
 # GLOBALS ###############
-#########################
-# declare -r FILE_NAME="$0"
-# declare -r FILE_NAME=$(basename $0)
-readonly PROGRAM="$0"
-readonly ARGS="$@"
 
-readonly APPLE_SCRIPTS="./support/scripts/apple_scripts/"
+declare -r PROGRAM="${0}"
+declare -ra ARGS=("${@}")
+
+declare -r APPLE_SCRIPTS='./support/scripts/apple_scripts/'
 # declare -r BASH_SCRIPTS="./support/scripts/bash/"
+declare -r TRUE='true'
+declare -r FALSE='false'
 
 
-
-
-#######################################################################################################
-#######################################################################################################
 # read paramters in ###################################################################################
-#######################################################################################################
-#######################################################################################################
+
 # parse ARG flags into readonly global variables
-function read_parameters(){
+function initialize_input(){
     log_func "${FUNCNAME[0]}"
     
-    local -r args="$@";
+    local -ra args=( "${@}" )
     
-    local is_dump_homebrew="false"
-    local is_run_mackup="false"
-    local is_run_arq="false"
-    local is_update_devonthink="false"
-    local is_help="false"
+    local -r is_dump_homebrew="${FALSE}"
+    local -r is_run_mackup="${FALSE}"
+    local -r is_run_arq="${FALSE}"
+    local -r is_update_devonthink="${FALSE}"
+    local -r is_help="${FALSE}"
 
-    local OPTS=$(getopt -o dish --long ,dump-homebrew,backup-mackup,run-arq,update-devonthink,help -- "$@");
-    eval set -- "$OPTS";
+    local -r opts=$(getopt -o dish --long ,dump-homebrew,backup-mackup,run-arq,update-devonthink,help -- "${args[@]}" );
+    eval set -- "${opts}";
     while true ; do
-        case "$1" in
+        case "${1}" in
             --dump-homebrew) 
-                is_dump_homebrew="true"
+                is_dump_homebrew="${TRUE}"
                 shift 1;
                 ;;
             --backup-mackup)
-                is_run_mackup="true"
+                is_run_mackup="${TRUE}"
                 shift 1;
                 ;;
             --run-arq)
-                is_run_arq="true"
+                is_run_arq="${TRUE}"
                 shift 1;
                 ;;
             --update-devonthink)
-                is_update_devonthink="true"
+                is_update_devonthink="${TRUE}"
                 shift 1;
                 ;;
             --help)
-                is_help="true"
+                is_help="${TRUE}"
                 shift 1;
                 ;;
             *)
@@ -68,80 +62,67 @@ function read_parameters(){
     done;
 
     # CREATES GLOBAL VARIABLES #####################
-    readonly IS_DUMP_HOMEBREW="$is_dump_homebrew"
-    readonly IS_RUN_MACKUP="$is_run_mackup"
-    readonly IS_RUN_ARQ="$is_run_arq"
-    readonly IS_UPDATE_DEVONTHINK="$is_update_devonthink"
-    readonly IS_HELP="$is_help"
+    readonly IS_DUMP_HOMEBREW="${is_dump_homebrew}"
+
+    readonly IS_RUN_MACKUP="${is_run_mackup}"
+    readonly IS_RUN_ARQ="${is_run_arq}"
+    readonly IS_UPDATE_DEVONTHINK="${is_update_devonthink}"
+    readonly IS_HELP="${is_help}"
     # CREATES GLOBAL VARIABLES #####################
 }
 
 
 
 
-#######################################################################################################
-#######################################################################################################
 # logging #############################################################################################
-#######################################################################################################
-#######################################################################################################
 
 function log(){
-    local -r msg="$1"
-    echo "$PROGRAM ===========> LOG: $msg"
+    local -r msg="${1}"
+    echo "${PROGRAM} ===========> LOG: ${msg}"
 }
 
 function log_func(){
-    local -r function_name="$1"
-    log "function - $function_name"
+    local -r function_name="${1}"
+    log "function - ${function_name}()"
 }
 
 
 
-#######################################################################################################
-#######################################################################################################
 # print the usage #####################################################################################
 # displays usage information to the user for this script ##############################################
 # http://docopt.org ###################################################################################
-#######################################################################################################
-#######################################################################################################
 
 function usage(){
     log_func "${FUNCNAME[0]}"
     
-    echo "
-    -----------------------------------------------------------------------------------------------------
-     Usage: $PROGRAM 
-            [--dump-homebrew --backup-mackup --run-arq --update-devonthink] | --help
-
-        --help                  HELP: displays this usage page
-
-        --dump-homebrew         dumps homebrew information (installed apps) to Brewfile.dump
-
-        --backup-mackup         run \`mackup backup\` to update backed up preference files
-
-        --run-arq               run arq backup
-
-        --update-devonthink     index and sync DevonThink
-
-        
-        
-     Need more info on this documentation? Visit http://docopt.org
-
-    -----------------------------------------------------------------------------------------------------"
+    echo
+    echo '-----------------------------------------------------------------------------------------------------'
+    echo "Usage: ${PROGRAM}"
+    echo '[--dump-homebrew --backup-mackup --run-arq --update-devonthink] | --help'
+    echo
+    echo '--help                  HELP: displays this usage page'
+    echo
+    echo '--dump-homebrew         dumps homebrew information (installed apps) to Brewfile.dump'
+    echo
+    echo '--backup-mackup         run `mackup backup` to update backed up preference files'
+    echo
+    echo '--run-arq               run arq backup'
+    echo
+    echo '--update-devonthink     index and sync DevonThink'
+    echo
+    echo
+    echo 'Need more info on this documentation? Visit http://docopt.org'
+    echo '-----------------------------------------------------------------------------------------------------'
 }
 
 
-#######################################################################################################
-#######################################################################################################
 # helper functions ####################################################################################
-#######################################################################################################
-#######################################################################################################
 
 function create_macos_popup(){
     log_func "${FUNCNAME[0]}"
     
-    local -r message="$1"
-    osascript -e "tell app \"System Events\" to display dialog \"$message\""
+    local -r message="${1}"
+    osascript -e "tell app \"System Events\" to display dialog \"${message}\""
 }
 
 
@@ -153,39 +134,29 @@ function authenticate_sudo(){
 }
 
 
-#######################################################################################################
-#######################################################################################################
 # # dumps all installed homebrew stuff into a Brewfile ################################################
-#######################################################################################################
-#######################################################################################################
 function dump_homebrew(){
     log_func "${FUNCNAME[0]}"
     
-    local -r dump_location="./support/resources/brew/Brewfile.dump"
-    brew bundle dump --force --file="$dump_location"
+    local -r dump_location='./support/resources/brew/Brewfile.dump'
+    brew bundle dump \
+        --force \
+        --file="${dump_location}"
 }
 
 
-#######################################################################################################
-#######################################################################################################
 # run devonthink index and sync apple scripts #########################################################
-#######################################################################################################
-#######################################################################################################
 
 function index_and_sync_devonthink(){
     log_func "${FUNCNAME[0]}"
-    (   cd "$APPLE_SCRIPTS" || return
-        osascript "index_devonthink.scpt"
-        osascript "sync_devonthink.scpt"
+    (   cd "${APPLE_SCRIPTS}" || return
+        osascript 'index_devonthink.scpt'
+        osascript 'sync_devonthink.scpt'
     )
 }
 
 
-#######################################################################################################
-#######################################################################################################
 # links any new apps to have pref files sym linked ####################################################
-#######################################################################################################
-#######################################################################################################
 function backup_mackup(){
     log_func "${FUNCNAME[0]}"
     mackup backup
@@ -194,20 +165,16 @@ function backup_mackup(){
 
 
 
-#######################################################################################################
-#######################################################################################################
 # run arq backup ######################################################################################
-#######################################################################################################
-#######################################################################################################
 function backup_arq(){
     log_func "${FUNCNAME[0]}"
     
-    (   cd "/Applications/Arq.app/Contents/MacOS" || return
+    (   cd '/Applications/Arq.app/Contents/MacOS' || return
     
-        create_macos_popup "The following two dialogs will prompt to backup with Arq.
+        create_macos_popup 'The following two dialogs will prompt to backup with Arq.
         1. Select Backblaze B2
         2. Select 10.0.1.8 to back up to the local Synology NAS
-            If remote, ensure VPN in enabled"
+            If remote, ensure VPN in enabled'
             
         # select B2
         ./Arq backupnow
@@ -218,17 +185,12 @@ function backup_arq(){
 }
 
 
-#######################################################################################################
-#######################################################################################################
 # main and init #######################################################################################
-#######################################################################################################
-#######################################################################################################
-
 function initialize(){
     log_func "${FUNCNAME[0]}"
-    read_parameters $ARGS
+    initialize_input "${ARGS[@]-}"
     
-    if [[ "$IS_HELP" == "true" ]]; then
+    if [[ "${IS_HELP}" == "${TRUE}" ]]; then
         usage
         exit 0
     fi
@@ -237,28 +199,29 @@ function initialize(){
     
 }
 
+
 function main(){
     log_func "${FUNCNAME[0]}"
 
     initialize
     
-    if [[ "$IS_DUMP_HOMEBREW" == "true" ]]; then
-        log "dump homebrew"
+    if [[ "${IS_DUMP_HOMEBREW}" == "${TRUE}" ]]; then
+        log 'dump homebrew'
         dump_homebrew
     fi
     
-    if [[ "$IS_RUN_MACKUP" == "true" ]]; then
+    if [[ "${IS_RUN_MACKUP}" == "${TRUE}" ]]; then
         log "backup mackup"
         backup_mackup
     fi
     
-    if [[ "$IS_UPDATE_DEVONTHINK" == "true" ]]; then
-        log "index and sync devonthink"
+    if [[ "${IS_UPDATE_DEVONTHINK}" == "${TRUE}" ]]; then
+        log 'index and sync devonthink'
         index_and_sync_devonthink
     fi
     
-    if [[ "$IS_RUN_ARQ" == "true" ]]; then
-        log "backup arg"
+    if [[ "${IS_RUN_ARQ}" == "${TRUE}" ]]; then
+        log 'backup arg'
         backup_arq
     fi
 }
